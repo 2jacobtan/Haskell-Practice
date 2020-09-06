c914124134983805257701e46e47eb6acb0fdae8
Jacob Tan En <jacob.tan.en@gmail.com>
2020-09-07 00:55:49 +0800

    First commit. Add Haskell Book: Chapter 11: Phone exercise

---
+++ HaskellBookCh11Phone.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE LambdaCase #-}
+import Data.Char (isUpper, toLower, toUpper)
+import Data.List (elemIndex, foldl')
+import Data.Maybe
+
+-- K is a normal key, KS is the * (star) key.
+data KeyPress = K Int  | KA deriving Show
+data KeyResult = R String | Caps deriving Show
+
+type DaPhone = [(KeyPress, KeyResult)]
+daPhone :: DaPhone
+daPhone = [
+    (K 1 , R ""),
+    (K 2 , R "abc2"),
+    (K 3 , R "def3"),
+    (K 4 , R "ghi4"),
+    (K 5 , R "jkl5"),
+    (K 6 , R "mno6"),
+    (K 7 , R "pqrs7"),
+    (K 8 , R "tuv8"),
+    (K 9 , R "wxyz9"),
+    (K 0 , R " "),
+    (K 12 , R ".,"),
+    (KA , Caps)
+    ]
+
+type PressCount = Int
+reverseTaps :: Char -> [(KeyPress, PressCount)]
+reverseTaps c =
+    prepend [key'count daPhone]
+    where
+        prepend = if isUpper c then \xs -> (KA, 1) : xs else id
+        ch = toLower c
+        key'count ((key , R chars):others) = case elemIndex ch chars of
+            Just n -> (key, n+1)
+            Nothing -> key'count others 
+encodeConvo string = reverseTaps <$> string
+
+makeTaps :: [(KeyPress, PressCount)] -> Char
+makeTaps = \case
+    [(KA,1),(key, count)] -> toUpper $ getChar (key, count)
+    [(key, count)] -> getChar (key, count)
+    where
+        getChar (key, count) =
+            go key count daPhone
+            where
+                go (K keyNum) c ((K keyNum', R string):others)
+                    | keyNum == keyNum' =  string !! (c-1)
+                    | otherwise = go (K keyNum) c others
+
+decodeConvo xs = makeTaps <$> xs
+
+digitsPressedCount message = countDigits $ encodeConvo message
+    where
+        countDigits encoded = sum $ getCount <$> encoded
+        getCount [(KA,_),(key,count)] = count
+        getCount [(key,count)] = count
+
+print' :: Show a => a -> IO ()
+print' = putStrLn . show
+main = do 
+    print' "Hello World!"
+    putStrLn ""
+    print' $ reverseTaps 'e'
+    print' $ makeTaps [(KA, 1),(K 3,2)]
+    print' $ makeTaps . reverseTaps $ 'F'
+    putStrLn ""
+    print' $ encodeConvo "Hello, world."
+    print' $ decodeConvo . encodeConvo $ "Hello, world."
+    putStrLn ""
+    print' $ digitsPressedCount "Hello, world."
+    print' $ digitsPressedCount "hELLO, WORLD."
