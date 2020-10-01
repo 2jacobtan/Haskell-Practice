1e516daa581f52d724ab7a511331e0ee7eb3b2ee
Jacob Tan En <jacob.tan.en@gmail.com>
2020-10-01 18:30:21 +0800

    Add to WYAS: parse escaped chars

--- WYAS/app/Main.hs
+++ WYAS/app/Main.hs
@@ -3,14 +3,15 @@ module Main where
 import Lib
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad (liftM)
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do
@@ -20,10 +21,47 @@ main = do
 spaces :: Parser ()
 spaces = skipMany1 space
 
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | String String
-             | Bool Bool
+data LispVal 
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool deriving Show
 
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (escapedChars <|> noneOf "\"")
+  char '"'
+  return $ String x
+  where
+    escapedChars
+      = char '\\'
+      >> (
+        (char '\\' >> pure '\\')
+        <|> (char '"' >> pure '"')
+        <|> (char 'n' >> pure '\n')
+
+      )
+      -- = (string "\\\\" >> pure '\\')
+      -- <|> (string "\\\"" >> pure '\"')
+      -- <|> (string "\\n" >> pure '\n')
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of 
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
