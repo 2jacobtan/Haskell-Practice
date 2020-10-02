b563393379116e7debccf6b832b2e8691d365178
Jacob Tan En <jacob.tan.en@gmail.com>
2020-10-02 23:43:16 +0800

    Add to HaskellBook ch14 Testing: QuickCheck demo

--- HaskellBook/ch14-testing/Addition/app/Main.hs
+++ HaskellBook/ch14-testing/Addition/app/Main.hs
@@ -2,9 +2,14 @@ module Main where
 
 import Lib
 import Test.Hspec
+import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
   describe "Addition" $ do
     it "1 + 1 is greater than 1" $ do
       (1+1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 0 > (x :: Int)
