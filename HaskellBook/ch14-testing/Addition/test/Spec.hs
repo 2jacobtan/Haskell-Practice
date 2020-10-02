2a85758a349c7e086e430d85619dc9e71efb19f2
Jacob Tan En <jacob.tan.en@gmail.com>
2020-10-03 01:41:26 +0800

    Add to HaskellBook ch14 Testing: QuickCheck demo; with NestedList monoid laws test: done.. copied to test/Spec.hs

--- HaskellBook/ch14-testing/Addition/test/Spec.hs
+++ HaskellBook/ch14-testing/Addition/test/Spec.hs
@@ -1,2 +1,53 @@
+-- copied from [root]/app/Main.hs to use with "stack test" command
+
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import Lib
+import NestedList
+import Test.Hspec
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1+1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
+
+    it "left identity" $ do
+      property $ prop_left_id @(NestedList ())
+    it "right identity" $ do
+      property $ prop_right_id @(NestedList ())
+    it "associativity" $ do
+      property $ prop_assoc @(NestedList ())
+
+
+instance Arbitrary a => Arbitrary (NestedList a) where
+  -- arbitrary = arbitrary >>= \x -> return $ Value x
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    elements
+      [ Nil
+      , Value x
+      , Cons (Value x) (Value y)
+      , Cons Nil Nil
+      , Cons (Value x) Nil
+      , Cons Nil (Value x)
+      ]
+
+
+prop_left_id :: (Monoid a, Show a, Eq a) => a -> Bool
+prop_left_id x = mempty <> x == x
+
+prop_right_id :: (Monoid a, Show a, Eq a) => a -> Bool
+prop_right_id x = x <> mempty == x
+
+prop_assoc :: (Monoid a, Show a, Eq a) => a -> a -> a -> Bool
+prop_assoc x y z = x <> (y <> z) == (x <> y) <> z
+
