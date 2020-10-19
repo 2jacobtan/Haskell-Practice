136eb96ea1faff448526a380165377421d9d89cc
Jacob Tan En <jacob.tan.en@gmail.com>
2020-10-19 13:18:32 +0800

    Reorganise into modules.

---
+++ WYAS/src/Evaluation.hs
@@ -0,0 +1,10 @@
+module Evaluation where
+
+import Parsing
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
