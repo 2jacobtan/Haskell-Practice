d412052b949ec17864833fa32719a93b322ef3e4
Jacob Tan En <jacob.tan.en@gmail.com>
2020-10-21 17:59:06 +0800

    Add to WYAS: completed chapter: Evaluation

--- WYAS/src/Evaluation.hs
+++ WYAS/src/Evaluation.hs
@@ -23,7 +23,15 @@ primitives = [("+", numericBinop (+)),
               ("/", numericBinop div),
               ("mod", numericBinop mod),
               ("quotient", numericBinop quot),
-              ("remainder", numericBinop rem)]
+              ("remainder", numericBinop rem),
+              ("symbol?" , unaryOp symbolp) ,
+              ("string?" , unaryOp stringp) ,
+              ("number?" , unaryOp numberp) ,
+              ("bool?", unaryOp boolp) ,
+              ("list?" , unaryOp listp),
+              ("symbol->string", unaryOp symbol2string),
+              ("string->symbol", unaryOp string2symbol)
+              ]              
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numericBinop op params = Number $ foldl1 op $ map unpackNum params
@@ -37,3 +45,24 @@ unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
 unpackNum (List [n]) = unpackNum n
 unpackNum _ = 0
 
+unaryOp :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+unaryOp f [v] = f v
+
+symbolp, numberp, stringp, boolp, listp :: LispVal -> LispVal
+symbolp (Atom _)   = Bool True
+symbolp _          = Bool False
+numberp (Number _) = Bool True
+numberp _          = Bool False
+stringp (String _) = Bool True
+stringp _          = Bool False
+boolp   (Bool _)   = Bool True
+boolp   _          = Bool False
+listp   (List _)   = Bool True
+listp   (DottedList _ _) = Bool False
+listp   _          = Bool False
+
+symbol2string, string2symbol :: LispVal -> LispVal
+symbol2string (Atom s)   = String s
+symbol2string _          = String ""
+string2symbol (String s) = Atom s
+string2symbol _          = Atom ""
