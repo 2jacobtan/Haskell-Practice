e4e14264511fcc826f6a26e05407006bc3acf163
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-03 13:56:15 +0800

    Add to ch21-traversable: Chapter exercises: Big

--- HaskellBook/ch21-traversable/Exercises/src/Lib.hs
+++ HaskellBook/ch21-traversable/Exercises/src/Lib.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
--- {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Lib where
 
@@ -25,7 +25,7 @@ instance Traversable List where
   -- | deliberately fail testing
   -- sequenceA (Cons x xs) = Cons <$> x <*> pure Nil
 instance Arbitrary a => Arbitrary (List a) where
-  arbitrary = genericArbitraryU
+  arbitrary = genericArbitrary (1 % 3 % ())
   -- arbitrary = frequency
   --   [
   --     (1, return Nil),
@@ -33,8 +33,11 @@ instance Arbitrary a => Arbitrary (List a) where
   --   ]
 instance EqProp a => EqProp (List a)
 
+
 newtype Identity a = Identity a
-  deriving (Eq, Ord, Show, Generic)
+  deriving (
+    -- Eq, Ord,
+    Show, Generic)
 instance Traversable Identity where
   traverse f (Identity a) = fmap Identity (f a)
 instance Foldable Identity where
@@ -45,3 +48,73 @@ instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = genericArbitraryU
   -- arbitrary = Identity <$> arbitrary
 instance EqProp a => EqProp (Identity a)
+
+
+newtype Constant a b = Constant { getConstant :: a }
+  deriving (Show, Generic)
+instance Traversable (Constant a) where
+  sequenceA (Constant x) = pure $ Constant x
+instance Foldable (Constant a) where
+  foldMap _ _ = mempty
+instance Functor (Constant a) where
+  fmap _ (Constant x) = Constant x
+instance Arbitrary a => Arbitrary (Constant a b) where
+  arbitrary = genericArbitraryU
+instance (EqProp a, EqProp b) => EqProp (Constant a b)
+
+
+data Optional a = Nada | Yep a
+  deriving (Show, Generic)
+instance Traversable Optional where
+  sequenceA Nada = pure Nada
+  sequenceA (Yep a) = Yep <$> a
+instance Foldable Optional where
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
+instance Functor Optional where
+  fmap _ Nada = Nada
+  fmap f (Yep a) = Yep $ f a
+instance Arbitrary a => Arbitrary (Optional a) where
+  arbitrary = genericArbitraryU
+instance EqProp a => EqProp (Optional a)
+
+
+data Three a b c = Three a b c
+  deriving (Show, Generic)
+instance Traversable (Three a b) where
+  sequenceA (Three x y z) = Three x y <$> z
+instance Foldable (Three a b) where
+  foldMap f (Three _ _ z) = f z
+instance Functor (Three a b) where
+  fmap f (Three x y z) = Three x y (f z)
+instance Arbitrary c => Arbitrary (Three Int Int c) where
+  arbitrary = genericArbitraryU
+instance EqProp c => EqProp (Three Int Int c)
+
+
+data Pair a b = Pair a b
+  deriving (Show, Generic)
+instance Traversable (Pair a) where
+  sequenceA (Pair y z) = Pair y <$> z
+instance Foldable (Pair a) where
+  foldMap f (Pair _ z) = f z
+instance Functor (Pair a) where
+  fmap f (Pair y z) = Pair y (f z)
+instance Arbitrary c => Arbitrary (Pair Int c) where
+  arbitrary = genericArbitraryU
+instance EqProp c => EqProp (Pair Int c)
+
+
+data Big a b = Big a b b
+  deriving (Show, Generic)
+instance Traversable (Big a) where
+  sequenceA (Big x y1 y2) = Big x <$> y1 <*> y2
+instance Foldable (Big a) where
+  foldMap f (Big _ y1 y2) = f y1 <> f y2
+instance Functor (Big a) where
+  fmap f (Big x y1 y2) = Big x (f y1) (f y2)
+instance Arbitrary b => Arbitrary (Big Int b) where
+  arbitrary = genericArbitraryU
+instance EqProp b => EqProp (Big Int b)
+
+
--- HaskellBook/ch21-traversable/Exercises/test/Spec.hs
+++ HaskellBook/ch21-traversable/Exercises/test/Spec.hs
@@ -19,3 +19,14 @@ main = do
   putStrLn "List"
   quickBatch . traversable $ (undefined :: Identity (Int, Int, [Int]))
   putStrLn "Identity"
+  quickBatch . traversable $ (undefined :: Constant Int (Int, Int, [Int]))
+  putStrLn "Constant"
+  quickBatch . traversable $ (undefined :: Optional (Int, Int, [Int]))
+  putStrLn "Optional"
+  quickBatch . traversable $ (undefined :: Three Int Int (Int, Int, [Int]))
+  putStrLn "Three"
+  quickBatch . traversable $ (undefined :: Pair Int (Int, Int, [Int]))
+  putStrLn "Pair" 
+  quickBatch . traversable $ (undefined :: Big Int (Int, Int, [Int]))
+  putStrLn "Big"
+  
