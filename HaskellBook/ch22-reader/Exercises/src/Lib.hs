901a9855834e8fef90a9f775298b11dca2d71f40
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-04 18:29:56 +0800

    Add to ch22-reader: Exercises: up to sequA

--- HaskellBook/ch22-reader/Exercises/src/Lib.hs
+++ HaskellBook/ch22-reader/Exercises/src/Lib.hs
@@ -1,6 +1,45 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Applicative
+-- import Data.Maybe
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+lookup :: Eq a => a -> [(a, b)] -> Maybe b
+lookup l = foldr f Nothing
+  where
+    f (a,b) xs
+      | a == l = Just b
+      | otherwise = xs
+
+xs = Lib.lookup 3 $ zip x y
+
+ys = Lib.lookup 6 $ zip y z
+
+zs = Lib.lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = Lib.lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = liftA2 (,) ys zs
+
+x3 = liftA2 (,) z' z'
+
+uncurry f (a,b) = f a b
+
+summed = Lib.uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = liftA2 (&&) (>3) (<8)
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe a m = maybe a id m
+
+sequA m = sequenceA [(>3), (<8), even] m
+s' = summed <$> ((,) <$> xs <*> ys)
+
--- HaskellBook/ch22-reader/Exercises/test/Spec.hs
+++ HaskellBook/ch22-reader/Exercises/test/Spec.hs
@@ -1,2 +1,26 @@
+import Lib
+
+p = putStrLn ""
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  print x1
+  print x2
+  print $ x3 3
+  p
+  print $ bolt 5
+  p
+  print $ fromMaybe 0 xs
+  print $ fromMaybe 0 zs
+  p
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
+  p
+  print $ sequence [(>3), (<8), even] 7
+  p
+  -- print $
