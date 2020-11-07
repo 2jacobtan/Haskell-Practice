ae462804602da95d680cda747fdde7d2848895e9
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-07 22:02:20 +0800

    Add to ch23-state: 23.6 Write State for yourself

--- HaskellBook/ch23-state/src/RandomExample2.hs
+++ HaskellBook/ch23-state/src/RandomExample2.hs
@@ -3,7 +3,8 @@ module RandomExample2 where
 
 import Control.Applicative (liftA3)
 import Control.Monad (replicateM)
-import Control.Monad.Trans.State
+-- import Control.Monad.Trans.State
+import WriteState
 import System.Random
 import RandomExample
 import qualified Debug.Trace as Debug
