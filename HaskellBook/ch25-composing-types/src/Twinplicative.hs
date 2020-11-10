ef70eeb6f99e4240df1601d41a9fbeea13131370
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-10 23:10:42 +0800

    Add to ch25-composing-types: IdentityT

--- HaskellBook/ch25-composing-types/src/Twinplicative.hs
+++ HaskellBook/ch25-composing-types/src/Twinplicative.hs
@@ -86,3 +86,45 @@ instance BiFunctor (Main.Either) where
     Main.Left a -> Main.Left $ f a
     Main.Right b -> Main.Right $ g b
 
+
+newtype Identity a = Identity { runIdentity :: a }
+  deriving (Eq, Show)
+
+newtype IdentityT f a =
+  IdentityT { runIdentityT :: f a}
+  deriving (Eq, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity $ f a
+
+instance
+  (Functor m) =>
+  Functor (IdentityT m)
+  where
+  fmap f (IdentityT fa) = IdentityT $ fmap f fa
+
+instance Applicative Identity where
+  pure = Identity
+  Identity f <*> Identity a = Identity $ f a
+
+instance
+  Applicative m =>
+  Applicative (IdentityT m)
+  where
+  pure = IdentityT . pure
+  IdentityT fab <*> IdentityT fa = IdentityT $ fab <*> fa
+
+instance Monad Identity where
+  return = pure
+
+  (>>=) (Identity a) = ($ a) -- "clever"
+  -- Identity a >>= f = f a -- clearer
+
+instance
+  Monad m =>
+  Monad (IdentityT m) where
+  return = pure
+
+  (IdentityT ma) >>= f = IdentityT $ ma >>= runIdentityT . f
+
+  
