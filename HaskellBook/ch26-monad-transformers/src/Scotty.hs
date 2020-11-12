8415fa21aa2ea29359a5e6afad410e8a2fe02a1a
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-12 17:00:57 +0800

    Add to ch26-monad-transformers: Scotty.hs

---
+++ HaskellBook/ch26-monad-transformers/src/Scotty.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Scotty where
+
+-- import Data.Monoid (mconcat)
+import Web.Scotty
+import Control.Monad.Trans.Class (MonadTrans(lift))
+import Control.Monad.Trans.Except (ExceptT(ExceptT))
+import Control.Monad.Trans.Reader (ReaderT(ReaderT))
+import Control.Monad.Trans.State.Strict (StateT(StateT))
+import Web.Scotty.Internal.Types (ActionT(ActionT))
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    lift $ putStrLn "hello"
+    ActionT . lift . lift . lift $ putStrLn "lift 3"
+    ActionT
+      . (ExceptT . fmap Right)
+      . liftReaderT
+      -- . lift
+      . (\m -> StateT (\s -> m >>= \a -> return (a, s)))
+      -- . (\m -> StateT (
+      --     \s -> do
+      --       a <- m
+      --       return (a, s)
+      --     )
+      --   )
+      $ putStrLn "lift ExceptT + ReaderT"
+    html $
+      mconcat
+        [ "<h1>Scotty, ",
+          beam,
+          " me up!</h1>"
+        ]
+
+
+liftReaderT :: m a -> ReaderT r m a
+liftReaderT = ReaderT . const
