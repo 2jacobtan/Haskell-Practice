d8ecad0f8950ae0d15de021cd0c678d44f8a1d7a
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-12 23:47:25 +0800

    Add to ch26-monad-transformers: Temporary extension of structure (Scotty.hs)

--- HaskellBook/ch26-monad-transformers/src/Scotty.hs
+++ HaskellBook/ch26-monad-transformers/src/Scotty.hs
@@ -4,17 +4,30 @@ module Scotty where
 
 -- import Data.Monoid (mconcat)
 
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Trans.Class (MonadTrans (lift))
 import Control.Monad.Trans.Except (ExceptT (ExceptT))
+import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Reader (ReaderT (ReaderT))
 import Control.Monad.Trans.State.Strict (StateT (StateT))
+import Data.Maybe (fromMaybe)
+import Data.Text.Lazy (Text)
 import Web.Scotty
 import Web.Scotty.Internal.Types (ActionT (ActionT))
-import Control.Monad.IO.Class (MonadIO(liftIO))
+
+param' :: Parsable a => Text -> MaybeT ActionM a
+param' k = MaybeT $
+  rescue (Just <$> param k)
+    (const (return Nothing))
+
+type Reco =
+  (Integer, Integer, Integer, Integer)
 
 main = scotty 3000 $ do
   get "/:word" $ do
-    beam <- param "word"
+    beam' <- runMaybeT $ param' "word"
+    let beam = fromMaybe "" beam'
+    
     lift $ putStrLn "hello"
     ActionT . lift . lift . lift $ putStrLn "lift 3"
     ActionT
@@ -30,6 +43,18 @@ main = scotty 3000 $ do
       --   )
       $ putStrLn "lift ExceptT + ReaderT"
     liftIO $ putStrLn "liftIO"
+
+    -- Temporary extension of structure
+    reco <- runMaybeT $ do
+      a <- param' "1"
+      liftIO $ print a
+      b <- param' "2"
+      c <- param' "3"
+      d <- param' "4"
+      (lift . lift) $ print b
+      return ((a, b, c, d) :: Reco)
+    liftIO $ print reco    
+    
     html $
       mconcat
         [ "<h1>Scotty, ",
