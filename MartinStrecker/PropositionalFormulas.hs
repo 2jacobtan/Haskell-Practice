cb53f997224d80d577466984bc49318885c7f940
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-13 14:11:18 +0800

    Add to Martin Strecker's exercise for Propositional Formulas: removeConst

--- MartinStrecker/PropositionalFormulas.hs
+++ MartinStrecker/PropositionalFormulas.hs
@@ -1,11 +1,50 @@
+{-# LANGUAGE LambdaCase #-}
+import Prelude hiding (and)
+-- import Data.Maybe (fromMaybe)
+-- import Control.Applicative (Alternative((<|>)))
+
 data Form
   = C Bool
   | Not Form
   | And Form Form
   | Or Form Form
   | V String
+  deriving (Eq, Show, Read)
 
 _ = ((V "a" `And` (Not (V "b"))) `Or` ((V "c") `Or` (C False)))
 
 _ = Or (And (V "a") (Not (V "b"))) (Or (V "c") (C False))
+
+-- removeConst :: Form -> Form
+-- removeConst expr@(And _ _) = and expr
+
+-- and expr@(And left right) = fromMaybe expr (and' left right <|> and' right left)
+-- and' (C True) f = Just f
+-- and' (C False) _ = Just $ C False
+-- and' _ _ = Nothing
+
+-- or expr@(Or left right) = fromMaybe expr (or' left right <|> or' right left)
+-- or' (C True) _ = Just $ C True
+-- or' (C False) f = Just $ f
+-- or' _ _ = Nothing
+
+removeConst :: Form -> Form
+removeConst = \case
+  And (C False) _ -> C False
+  And (C True) f -> f
+  And _ (C False) -> C False
+  And f (C True) -> f
+  expr@(And _ _) -> expr -- optional
+  
+  Or (C False) f -> f
+  Or (C True) _ -> C True
+  Or f (C False) -> f
+  Or _ (C True) -> C True
+  expr@(Or _ _) -> expr -- optional
+
+  Not (C False) -> C True
+  Not (C True) -> C False
+  expr@(Not _) -> expr -- optional
+
+  expr -> expr
 
