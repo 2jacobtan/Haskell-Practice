7698056c2056469ec7c4ee3fd03f7752519e5695
Jacob Tan En <jacob.tan.en@gmail.com>
2020-11-19 09:36:17 +0800

    Add ch28-basic-libraries: Vector heap profiling seems to be working.

--- HaskellBook/ch28-basic-libraries/app/Main.hs
+++ HaskellBook/ch28-basic-libraries/app/Main.hs
@@ -4,36 +4,16 @@ import Criterion.Main
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
 
+unboxed _ = VU.fromList @Int [(0) .. 9999]
+{-# SCC unboxed #-}
+
+boxed _ = V.fromList @Int [0 .. 9999]
+{-# SCC boxed #-}
 
 main = do
   defaultMain
-    [ bench "bench1" $ whnf print $ 1
+    [ bench "bench1" $ whnf print $
+        {-# SCC printUnboxed #-} (unboxed ()) VU.! 1,
+      bench "bench2" $ whnf print $
+        {-# SCC printBoxed #-} (boxed ()) V.! 2
     ]
-  
-  let unboxed _ = VU.fromList @Int [(0) .. 9999]
-      {-# SCC unboxed #-}
-  
-  print $ {-# SCC "printUnboxed" #-} (unboxed ()) VU.! 1
-
-  defaultMain
-    [ bench "bench2" $ whnf print $ 2
-    ]
-  
-  print $ {-# SCC "printUnboxed" #-} (unboxed ()) VU.! 1
-  
-  
-  defaultMain
-    [ bench "bench3" $ whnf print $ 3
-    ]
-  
-  
-  let boxed _ = V.fromList [0 .. 9999]
-      {-# SCC boxed #-}
-  
-  print $ {-# SCC printBoxed #-} (boxed ()) V.! 2
-  
-  defaultMain
-    [ bench "bench4" $ whnf print $ 4
-    ]
-  
-  print $ {-# SCC printBoxed #-} (boxed ()) V.! 2
