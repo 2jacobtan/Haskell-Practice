ad36ce845f1aabfb5fca26b71a72e47b70e6e7ee
2jacobtan <jacob.tan.en@gmail.com>
2020-12-31 20:16:05 +0800

    WYAS: Defining Scheme Functions: done.

---
+++ WYAS/src/DefiningSchemeFunctions.hs
@@ -0,0 +1,10 @@
+module DefiningSchemeFunctions where
+
+import Types
+import VarsAndAssignment (nullEnv,  bindVars, liftThrows, IOThrowsError )
+import Evaluation (primitives, eval)
+
+primitiveBindings :: IO Env
+primitiveBindings = ($ map makePrimitiveFunc primitives) . bindVars =<< nullEnv 
+-- primitiveBindings = nullEnv >>= flip bindVars (map makePrimitiveFunc primitives)
+  where makePrimitiveFunc (var, func) = (var, PrimitiveFunc func)
