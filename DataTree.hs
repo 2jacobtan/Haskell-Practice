2e7b01201f20b12c5dc19c73ed398be801d25db7
2jacobtan <jacob.tan.en@gmail.com>
2021-02-26 01:42:01 +0800

    DataTree.hs: add output

--- DataTree.hs
+++ DataTree.hs
@@ -46,3 +46,140 @@ main :: IO ()
 main = do
   mapM_ (putStrLn . unpack . linearise) example_
   mapM_ (putStrLn . drawTree . fmap unpack) example_
+
+{-
+
+[1 of 1] Compiling DataTree         ( /home/jt2/repos/Haskell-Practice/DataTree.hs, interpreted )
+Ok, one module loaded.
+*DataTree PPrint> main
+I may order strawberry ice cream on Saturdays
+I may order strawberry ice cream on Sundays
+I may order strawberry cake on Saturdays
+I may order strawberry cake on Sundays
+I may order chocolate ice cream on Saturdays
+I may order chocolate ice cream on Sundays
+I may order chocolate cake on Saturdays
+I may order chocolate cake on Sundays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- strawberry
+   |     |
+   |     `- ice cream
+   |
+   `- Saturdays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- strawberry
+   |     |
+   |     `- ice cream
+   |
+   `- Sundays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- strawberry
+   |     |
+   |     `- cake
+   |
+   `- Saturdays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- strawberry
+   |     |
+   |     `- cake
+   |
+   `- Sundays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- chocolate
+   |     |
+   |     `- ice cream
+   |
+   `- Saturdays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- chocolate
+   |     |
+   |     `- ice cream
+   |
+   `- Sundays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- chocolate
+   |     |
+   |     `- cake
+   |
+   `- Saturdays
+MAY_
+|
++- I
+|
+`- ON_
+   |
+   +- ORDER_
+   |  |
+   |  `- ADJ_
+   |     |
+   |     +- chocolate
+   |     |
+   |     `- cake
+   |
+   `- Sundays
+*DataTree PPrint>
+
+-}
