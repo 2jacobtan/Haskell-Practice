fffcf6ef6eed76aca1fb03c3e99aaa88a02b5393
jt2@Shiversphere <jacob.tan.en@gmail.com>
2021-06-06 08:17:07 +0800

    add Haskell traverseSet.

--- 2021-06-.hs
+++ 2021-06-.hs
@@ -1,15 +1,18 @@
-import Data.Set (Set, fromList, singleton, toList)
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE EmptyDataDeriving #-}
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 -- https://twitter.com/chris__martin/status/1401242486702428164
 
-traverseSet' :: (Foldable t, Monoid (f (Set a1)), Functor f) =>
-  (a2 -> f a1) -> t a2 -> f (Set a1)
-traverseSet' f = foldMap (fmap singleton . f)
-
 traverseSet :: (Ord a1, Applicative f) =>
   (a2 -> f a1) -> Set a2 -> f (Set a1)
-traverseSet f = fmap fromList . traverse f . toList
+traverseSet f = fmap Set.fromList . traverse f . Set.toList
 
+traverseSet' :: (Foldable t, Monoid (f (Set a1)), Functor f) =>
+  (a2 -> f a1) -> t a2 -> f (Set a1)
+traverseSet' f = foldMap (fmap Set.singleton . f)
 
 -- (Applicative f, Monoid a) => Monoid (Ap f a)
 --- ^ This instance exists in:
@@ -19,5 +22,27 @@ traverseSet f = fmap fromList . traverse f . toList
 -- instance (Applicative f, Monoid a) => Monoid (f a)
 -- is defined for every applicative f.
 
-------------
+-- my version
+-- kinda bad because of Ord (f a1) constraint
+traverseSet'' :: (Applicative f, Ord a1, Ord (f a1)) =>
+  (a2 -> f a1) -> Set a2 -> f (Set a1)
+traverseSet'' f = foldr ((<*>) . fmap Set.insert) (pure mempty) . Set.map f
 
+--- | Even though `Ord a1` is not an explicit constraint in type signature of  traverseSet', it's still required. Cuz (<>) = union, which requires Ord.
+-- x = traverseSet' (pure @Maybe . pure @IO) (Set.fromList [1..3 :: Int])
+
+zz :: Set (IO Bool)
+zz = Set.singleton . pure @IO $ True
+
+-- zzz :: Set (IO Bool)
+-- zzz = zz <> undefined  
+
+data Null deriving Show
+
+yy :: Set Null
+yy = Set.empty @Null
+
+-- yyy :: Set Null
+-- yyy = yy <> undefined
+
+------------
