b8f8981145074ec312d98f085aa2b2785fb461fc
jt2@Shiversphere <jacob.tan.en@gmail.com>
2021-06-27 02:31:33 +0800

    @rae: Existentials and writing functions for length-indexed vectors

--- RaeHaskell.hs
+++ RaeHaskell.hs
@@ -10,9 +10,27 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
 
-import Data.Kind (Type)
-import Prelude (Bool (..), Show, (&&))
+-- https://www.youtube.com/watch?v=PHS3Q-tRjFQ&list=PLyzwHTVJlRc9QcF_tdqc9RdxJED8Mvyh1&index=26
+-- @rae: How to program in types with length-indexed vectors: Part 1
+-- {-# OPTIONS_GHC -Wall #-}
+-- {-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+import Data.Kind (Type, Constraint)
+import Prelude (Bool (..), Show, (&&), otherwise, ($), Eq ((==)), (||))
 
 data Nat = Zero | Succ Nat
 
@@ -83,3 +101,47 @@ drop :: SNat n -> Vec m a -> Vec (Drop n m) a
 drop SZero xs = xs
 drop _ Nil = Nil
 drop (SSucc n) (_ :> xs) = drop n xs
+
+type EVec :: (Nat -> Constraint) -> Type -> Type
+data EVec c a where
+  MkEVec :: c n => Vec n a -> EVec c a
+
+type KnowNothing :: Nat -> Constraint
+class KnowNothing n
+instance KnowNothing n
+
+type (>=) :: Nat -> Nat -> Constraint
+class (>=) n m
+instance n >= Zero
+instance m >= n => Succ m >= Succ n
+instance {-# OVERLAPPABLE #-} m >= n => Succ m >= n
+
+
+filter :: (a -> Bool) -> Vec n a -> EVec ((>=) n) a
+filter _ Nil = MkEVec Nil
+filter p (x:>xs)
+  | p x = case filter p xs of MkEVec tail ->  MkEVec $ x :> tail
+  | otherwise = case filter p xs of MkEVec v -> MkEVec v
+
+elem :: Eq a => a -> Vec n a -> Bool
+elem _ Nil = False
+elem x (y :> ys) = x == y || elem x ys
+
+type (<=) :: Nat -> Nat -> Constraint
+class (<=) n m
+instance {-# incoherent #-} Zero <= m
+instance {-# incoherent #-} n <= n
+instance {-# incoherent #-} n <= m => n <= Succ m
+-- instance {-# OVERLAPPABLE #-} n <= m => n <= Succ m
+
+type (<&&>) :: (Nat -> Constraint) -> (Nat -> Constraint) -> Nat -> Constraint
+class (cond1 n, cond2 n) => (<&&>) cond1 cond2 n
+instance (cond1 n, cond2 n) => (cond1 <&&> cond2) n
+
+-- union :: Eq a => Vec n a -> Vec m a -> EVec ((<=) n <&&> (<=) m) a
+union :: Eq a => Vec n a -> Vec m a -> EVec ((<=) m) a
+union Nil ys = MkEVec ys
+union xs Nil = MkEVec xs
+union (x :> xs) ys
+  | x `elem` ys = case xs `union` ys of MkEVec v -> MkEVec v
+  | otherwise = case xs `union` ys of MkEVec v -> MkEVec $ x :> v
