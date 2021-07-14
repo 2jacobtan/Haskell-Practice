a8c1199f5ba52824b07d80cd9507e26cd1b1302b
2jacobtan <jacob.tan.en@gmail.com>
2021-07-14 22:48:27 +0800

    witch library test

--- witch-library/src/WitchTest.hs
+++ witch-library/src/WitchTest.hs
@@ -8,15 +8,11 @@
 {-# LANGUAGE UndecidableInstances #-}
 module WitchTest where
 
-import Witch ( From (from), into )
+import Witch ( From (from), into, via )
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 -- https://hackage.haskell.org/package/witch-0.3.4.0/docs/Witch.html
-
-newtype Integ = Integ Int deriving (Eq, Ord)
-instance From Integ Int
-instance From Int Integ
 
 newtype List a = List [a]
   deriving stock Show
@@ -27,27 +23,34 @@ instance From [a] (List a)
 instance {-# OVERLAPPABLE #-}
   From [a] b  -- undecidable instance
   => From (List a) b where
-  from = into . into @[a]
+  -- from = into . into @[a]
+  from = via @[a]
 instance {-# OVERLAPPABLE #-}
   From b [a]  -- undecidable instance
   => From b (List a) where
-  from = into . into @[a]
+  -- from = into . into @[a]
+  from = via @[a]
 
-x :: Integer
-x = from (42 :: Int)  -- Int to Integ to Integer
+-- Taylor Fausak's response
+-- alternative to above instances
+-- https://www.reddit.com/r/haskell/comments/oje9k9/cast_haskell_values_with_witch/h55nmu2/?utm_source=reddit&utm_medium=web2x&context=3
+-- instance Ord a => From (List a) (Set a) where
+--   from = via @[a]
+-- instance From (Set a) (List a) where
+--   from = via @[a]
 
-y :: List Bool
-y = from [True]
+x :: List Bool
+x = from [True]
 
-z :: Set Integer
-z = into @(Set _) @(List _) . fmap (into @Integer @Int) $ List [1,2,3 :: Int]
+y :: Set Integer
+y = into @(Set _) @(List _) . fmap (into @Integer @Int) $ List [1,2,3 :: Int]
 
-zz :: List Integer
-zz = into @(List _) @(Set _) . Set.map (into @Integer @Int) $ Set.fromList [1,2,3 :: Int]
+z :: List Integer
+z = into @(List _) @(Set _) . Set.map (into @Integer @Int) $ Set.fromList [1,2,3 :: Int]
 
 -- >>> x
 -- >>> y
 -- >>> z
--- 42
 -- List [True]
 -- fromList [1,2,3]
+-- List [1,2,3]
