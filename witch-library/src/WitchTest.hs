6e61d0c4484746c3d70d2831a02a4b0b8ecfaac3
2jacobtan <jacob.tan.en@gmail.com>
2021-07-14 19:26:06 +0800

    witch library test

--- witch-library/src/WitchTest.hs
+++ witch-library/src/WitchTest.hs
@@ -1,23 +1,40 @@
-{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
--- {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+-- {-# OPTIONS_GHC -Wno-orphans#-}
+{-# LANGUAGE UndecidableInstances #-}
 module WitchTest where
 
-import Witch ( From (from) )
+import Witch ( From (from), into )
+import Data.Set (Set)
 
--- https://hackage.haskell.org/package/witch-0.3.4.0/docs/Witch.html#g:2
-newtype Name = Name String
-instance From Name String
-instance From String Name
+-- https://hackage.haskell.org/package/witch-0.3.4.0/docs/Witch.html
 
-newtype List a = List [a]
+newtype List a = List [a] deriving newtype Functor deriving Show
 instance From (List a) [a]
 instance From [a] (List a)
+instance {-# OVERLAPPABLE #-} From [a] b => From (List a) b where
+  from = into . into @[a]
 
-newtype Integ = Integ Int
+newtype Integ = Integ Int deriving (Eq, Ord)
 instance From Integ Int
 instance From Int Integ
 
 x :: Integer
-x = from (2 :: Int)  -- Int to Integ to Integer
+x = from (42 :: Int)  -- Int to Integ to Integer
+
+y :: List Bool
+y = from [True]
+
+z :: Set Integer
+z = into . fmap (into @Integer) $ List [1,2,3 :: Int]
+
+-- >>> x
+-- >>> y
+-- >>> z
+-- 42
+-- List [True]
+-- fromList [1,2,3]
