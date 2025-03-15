e2ecf66a9dab2894642b32b9a86de210a76035c7
Jacob Tan En <jacob.tan.en@gmail.com>
2025-03-15 11:59:30 +0800

    fixDef with Int instead of type-level peano  Nat

--- sandbox/src/FixClass.hs
+++ sandbox/src/FixClass.hs
@@ -6,20 +6,24 @@ module FixClass where
 data Nat = Z | S Nat
 
 class Fix (n::Nat) where
-    fix :: (a -> a) -> a
+    fixDef :: a -> (a -> a) -> a
 
 instance Fix Z where
-  fix f = f undefined
+  fixDef d f = d
 
 instance Fix n => Fix (S n) where
-  fix f = f (fix @n f)
+  fixDef d f = f (fixDef @n d f)
 
 sum' :: ([Int] -> Int) -> [Int] -> Int
-sum' loop = g loop
-  where
-  g loop rest = case rest of 
-    [] -> 0
-    x:xs -> x + loop xs
+sum' rec rest = case rest of
+  [] -> 0
+  x:xs -> x + rec xs
 
-x = fix @(  (S (S Z))) sum' [1..3]
-y = fix @(S (S (S Z))) sum' [1..3]
+x = fixDef @(  (S (S Z))) (const 0) sum' [1..3]
+y = fixDef @(S (S (S Z))) (const 0) sum' [1..3]
+
+fixDef' 0 d f = d
+fixDef' n d f = f (fixDef' (n-1) d f)
+
+x' = fixDef' 2 (const 0) sum' [1..3]
+y' = fixDef' 3 (const 0) sum' [1..3]
